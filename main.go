bdbf188411b83d97be88b2bff17bbf7445599702
Rodrigo Machado <rcmachado@gmail.com>
2018-06-15 09:58:33 -0300

    Remove commented code

--- main.go
+++ main.go
@@ -5,18 +5,3 @@ import "github.com/rcmachado/changelog/cmd"
 func main() {
 	cmd.Execute()
 }
-
-/*func main() {
-	input, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	extensions := blackfriday.NoIntraEmphasis | blackfriday.NoEmptyLineBeforeBlock | blackfriday.Footnotes
-
-	renderer := &keepachangelog.Renderer{}
-
-	output := blackfriday.Run(input, blackfriday.WithExtensions(extensions), blackfriday.WithRenderer(renderer))
-	fmt.Printf("%s", output)
-}*/
