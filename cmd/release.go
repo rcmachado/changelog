dc517e07560dd91c0b2a54a9f6a0ebc542532612
Rodrigo Machado <rcmachado@gmail.com>
2018-08-05 14:48:03 -0300

    Refactor release command

--- cmd/release.go
+++ cmd/release.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"os"
 	"time"
 
@@ -17,31 +18,17 @@ var releaseDate string
 var compareURL string
 
 var releaseCmd = &cobra.Command{
-	Use:   "release",
-	Short: "Bump version on changelog",
-	Long:  "Change current Unreleased version into the new version",
-	Args:  cobra.ExactArgs(1),
+	Use:   "release [version]",
+	Short: "Change Unreleased to [version]",
+	Long: `Change Unreleased section to [version], updating the compare links accordingly.
+It will normalize the output with the new version.
+`,
+	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		input := readChangelog()
 
-		version := chg.Version{
-			Name: args[0],
-			Date: releaseDate,
-		}
-
-		if compareURL != "" {
-			version.Link = compareURL
-		}
-
-		changelog := parser.Parse(input)
-		_, err := changelog.Release(version)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to bump version to %s: %s\n", args[0], err)
-			os.Exit(3)
-		}
-
 		var buf bytes.Buffer
-		changelog.Render(&buf)
+		release(input, args, &buf)
 		output := buf.Bytes()
 
 		writeChangelog(output)
@@ -50,7 +37,27 @@ var releaseCmd = &cobra.Command{
 
 func init() {
 	today := time.Now().Format(dateFormat)
-	releaseCmd.Flags().StringVarP(&releaseDate, "release-date", "d", today, "")
+	releaseCmd.Flags().StringVarP(&releaseDate, "release-date", "d", today, "Release date")
 	releaseCmd.Flags().StringVarP(&compareURL, "compare-url", "c", "", "Overwrite compare URL for Unreleased section")
 	rootCmd.AddCommand(releaseCmd)
+}
+
+func release(input []byte, args []string, w io.Writer) {
+	version := chg.Version{
+		Name: args[0],
+		Date: releaseDate,
+	}
+
+	if compareURL != "" {
+		version.Link = compareURL
+	}
+
+	changelog := parser.Parse(input)
+	_, err := changelog.Release(version)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to create release '%s': %s\n", args[0], err)
+		os.Exit(3)
+	}
+
+	changelog.Render(w)
 }
