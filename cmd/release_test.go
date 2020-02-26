5b5bc8f6152799dab7abd82f667f38b0321f5c4b
Eduardo Cardoso <dudubet@gmail.com>
2020-02-26 15:33:50 -0300

    Add cmd test for release without compare url

--- cmd/release_test.go
+++ cmd/release_test.go
@@ -46,7 +46,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 }
 
 func TestReleaseCmdError(t *testing.T) {
-	changelog, err := ioutil.ReadFile("testdata/minimal-changelog.md")
+	changelog, err := ioutil.ReadFile("testdata/minimal-changelog-no-url.md")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -62,4 +62,42 @@ func TestReleaseCmdError(t *testing.T) {
 	_, err = release.ExecuteC()
 
 	assert.Error(t, err)
+}
+
+func TestReleaseCmdNoCompareURL(t *testing.T) {
+	changelog, err := ioutil.ReadFile("testdata/minimal-changelog.md")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := `# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
+and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+## [0.1.0] - 2018-06-18
+### Added
+- Item 1
+
+[Unreleased]: https://github.com/rcmachado/changelog/compare/0.1.0...HEAD
+[0.1.0]: https://github.com/rcmachado/changelog/compare/ae761ff...0.1.0
+`
+
+	out := new(bytes.Buffer)
+	iostreams := &IOStreams{
+		In:  bytes.NewBuffer(changelog),
+		Out: out,
+	}
+
+	release := newReleaseCmd(iostreams)
+	// Missing --compare-url, as the autodetect won't work for the minimal changelog
+	release.SetArgs([]string{"0.1.0", "--release-date", "2018-06-18"})
+	_, err = release.ExecuteC()
+
+	assert.Nil(t, err)
+	assert.Equal(t, expected, string(out.Bytes()))
 }
